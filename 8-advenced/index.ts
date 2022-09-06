0f56525088f3cbb115d90b3ee5810d3922dc93a4
thesmartuz <109833765+thesmartuz@users.noreply.github.com>
2022-09-06 11:04:34 -0700

    Course has been ended!

--- 8-advenced/index.ts
+++ 8-advenced/index.ts
@@ -2,13 +2,28 @@ let a = "Hello world";
 if(typeof a == 'string'){
 	console.log(a)
 }
-
 let b: typeof a = "Hello world" // other types not working
 
 
-// IN THE TYPE
+// KEYOF
 type Coord  = {
 	lat: number,
 	long: number
-
 }
+
+type P = keyof Coord
+let obj: P = "lat"
+
+
+
+// TYPE NULL
+function log(a: string | null){
+	// ? => optional chayning
+	if(!a){
+		return a?.toLowerCase()
+	}
+}
+
+
+// TYPE BININT
+const c: bigint = BigInt(111)
